8dafcad0f0645305e638d18af348dfdde7bdeacb
amarjeet krishnan <63232182+amarjeet4296@users.noreply.github.com>
2022-03-14 16:16:38 +0530

    Update Dictionary1.py

--- Dictionary/Dictionary1.py
+++ Dictionary/Dictionary1.py
@@ -11,3 +11,17 @@ print(currency['India'])
 #2. Calling all values in a dictionary
 for k, v in currency.items():
     print(k, " = " , v)
+    
+#Adding new key, value in a dictionary
+currency['Polland'] = 'Zolty'
+
+#Create an empty dictionary
+country_currency = {}
+
+#wipe out an existing dictionary
+'''currency = {}'''
+
+#Loop in the dictionary
+for key in currency:
+    print(key)
+    print(currency[key])
